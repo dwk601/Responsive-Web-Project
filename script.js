8b3f3fb6163339f3802d3beb3f3eba32f7a04074
dwk601 <10829539@uvu.edu>
2022-10-30 19:34:06 -0600

    change the pictures, and hover effect, hide/show content.

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+var figure = $(".video").hover(hoverVideo, hideVideo);
+
+function hoverVideo(e) {
+  $("video", this).get(0).play();
+}
+
+function hideVideo(e) {
+  $("video", this).get(0).pause();
+}
