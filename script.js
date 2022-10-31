84e5e8d596406cc3b86efc0dbd61c6c1167f6651
dwk601 <10829539@uvu.edu>
2022-10-30 19:58:05 -0600

    mousehover video plays, mouseout video pause and reset.

--- script.js
+++ script.js
@@ -7,13 +7,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-var figure = $(".video").hover(hoverVideo, hideVideo);
-
-function hoverVideo(e) {
-  $("video", this).get(0).play();
-}
-
-function hideVideo(e) {
-  $("video", this).get(0).pause();
-}
