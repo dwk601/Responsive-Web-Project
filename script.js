d22731ddcee0e458bdeb842b15893ca673e2dacd
dwk601 <108056780+dwk601@users.noreply.github.com>
2022-11-16 17:38:05 -0700

    CSS theme, added a theme-toggle checkbox to switch from dark to light theme.

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+const themeToggle = document.querySelector("#theme-toggle");
+const body = document.querySelector("body");
+
+themeToggle.addEventListener("change", function () {
+  if (this.checked) {
+    // if toggle is on then add dark class to body
+    body.style.backgroundColor = "#fff";
+    body.style.color = "#000";
+  } else {
+    // if toggle is off then remove dark class from body
+    body.style.backgroundColor = "#333";
+    body.style.color = "#fff";
+  }
+});
